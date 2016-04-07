a443504f14898f29d144027ed7e538e726582704
Vista <Elbar@users.noreply.github.com>
2016-04-07 10:33:38 +0600

    Updated index.php

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
 		  <div id="nav-menu-left"></div>
 		  <div id="nav-menu">
 		  <!-- start of navigation -->
-		    <ul>
-			  <li><a href="#">Home</a></li>
-			  <li><a href="#">Products</a></li>
-			  <li><a href="#">Services</a></li>
-			  <li><a href="#">Terms</a></li>
-			  <li><a href="#">FAQ</a></li>
-			  <li><a href="#" style="background-image: none;">Contact Us</a></li>
-			</ul>
+		 <!--   <ul>-->
+			<!--  <li><a href="#">Home</a></li>-->
+			<!--  <li><a href="#">Products</a></li>-->
+			<!--  <li><a href="#">Services</a></li>-->
+			<!--  <li><a href="#">Terms</a></li>-->
+			<!--  <li><a href="#">FAQ</a></li>-->
+			<!--  <li><a href="#" style="background-image: none;">Contact Us</a></li>-->
+			<!--</ul>-->
 			<!-- end navigation -->
           </div>
 		  <div id="nav-menu-right"></div>
@@ -45,7 +45,7 @@
 		  <!-- page content below -->
 	      <h1>Education is powerfull weapon</h1>
          
-							<p>UpgradedU is an online course provider, where smart and initiative young people explain various ìtoughî subjects in an understandable, straightforward and unchallenging ways.</p>
+							<p>UpgradedU is an online course provider, where smart and initiative young people explain various ‚Äútough‚Äù subjects in an understandable, straightforward and unchallenging ways.</p>
 							<p>Platform covers major courses taught at universities. Moreover, it is a collaboration of people who care about the future generations.<br />
 		</div>
 	  </div>
