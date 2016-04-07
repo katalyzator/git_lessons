19ee261b5af5ad1aabc9211f015341f99aa74378
Katalyzator <web.coder96@gmail.com>
2016-04-07 10:27:35 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+	<title>UpgradedU</title>
+	<link rel="stylesheet" media="screen" type="text/css" href="style.css" />
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+  </head>
+
+  <body>
+    <div id="page">
+	  <div id="header">
+	    <div id="logo">
+		<!-- type your logo and small slogan here -->
+		<p>UpgradedU<span class="green"></span></p>
+		
+		<!-- end logo -->
+		</div>
+		<div id="nav">
+		  <div id="nav-menu-left"></div>
+		  <div id="nav-menu">
+		  <!-- start of navigation -->
+		    <ul>
+			  <li><a href="#">Home</a></li>
+			  <li><a href="#">Products</a></li>
+			  <li><a href="#">Services</a></li>
+			  <li><a href="#">Terms</a></li>
+			  <li><a href="#">FAQ</a></li>
+			  <li><a href="#" style="background-image: none;">Contact Us</a></li>
+			</ul>
+			<!-- end navigation -->
+          </div>
+		  <div id="nav-menu-right"></div>
+		</div>
+	  </div>
+	  <div class="clearfloats"></div>
+	  <div id="header2">
+
+	    <!--<div id="header2-slogan1"><p>a large website slogan</p></div>-->
+	    <!--<div id="header2-slogan2"><p>can go in this area</p></div>-->
+	    <!-- end header slogan -->
+	  </div>
+	  
+	  <div id="content">
+	    <div class="content-padding">
+		  <!-- page content below -->
+	      <h1>Education is powerfull weapon</h1>
+         
+							<p>UpgradedU is an online course provider, where smart and initiative young people explain various “tough” subjects in an understandable, straightforward and unchallenging ways.</p>
+							<p>Platform covers major courses taught at universities. Moreover, it is a collaboration of people who care about the future generations.<br />
+		</div>
+	  </div>
+	  <div id="body-divider"></div><!-- the division between the light green content section, and the dark -->
+	  <div id="content2">
+	    <div class="content-padding">
+		  <!-- content blocks (three side by side), can remove if needed, this was just an example -->
+		  <div class="content2-block">
+		    <h2>News Feeder</h2>
+		    <p>We opened new Enlish course <br />
+							
+						
+		  </div>
+		  <div class="content-divider"></div>
+		  <div class="content2-block">
+		    <h3>Users comments</h3>
+		   <p>This a very good opportunity <br />
+							
+		  </div>
+		  <div class="content-divider"></div>
+  		  <div class="content2-block">
+		    <h4>Newsletter</h4>
+			<p>Subscribe to our monthly newsletter below:</p>
+			<div id="subscribe">
+              <form action="yourformmailhere" enctype="multipart/form-data" method="post">
+                <input type="hidden" name="sendtoemail" value="youremailaddress" />
+                <input type="hidden" name="redirect" value="yourwebsiteaddress" />
+                <input type="hidden" name="subject" value="Newsletter subscription from your website" />
+                <input name="name" type="text" value="YOUR NAME" class="inputstyle" />
+		        <input name="email" type="text" value="YOUR EMAIL ADDRESS" class="inputstyle" />
+		        <input type="submit" value="subscribe" class="button" />
+		      </form>
+		    </div>
+		  <!-- end content blocks -->
+		  </div>
+		  <div class="clearfloats"></div>
+		</div>
+      </div>
+	  <div id="footer-div"></div>
+	  <div id="footer">
+	    <p> &copy; UPGRADEDU 2016, All rights reserved.</p>
+	  </div>
+	</div>
+  </body>
+</html>
